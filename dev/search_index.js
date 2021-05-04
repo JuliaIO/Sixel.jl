b97061e1da9c4dd1f3f5a0ce855c6f1bfe19feb1
Documenter.jl <documenter@juliadocs.github.io>
2021-05-04 10:47:54 +0000

    build based on 2876d7c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sixel","category":"page"},{"location":"#Sixel","page":"Home","title":"Sixel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sixel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sixel]","category":"page"}]
+}
